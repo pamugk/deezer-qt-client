1c44601e341cadb6165cdab4b5219e65d7fb712f
Alexander Permyakov <pamugk@gmail.com>
2021-06-06 20:43:36 +0500

    Basic app layout
    Designed basic application layout

--- deezer-qt-client_ru_RU.ts
+++ deezer-qt-client_ru_RU.ts
@@ -4,64 +4,80 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="26"/>
-        <source>MainWindow</source>
+        <location filename="mainwindow.ui" line="31"/>
+        <source>Неофициальный клиент Deezer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="289"/>
-        <source>sliders-h</source>
+        <location filename="mainwindow.ui" line="366"/>
+        <location filename="mainwindow.ui" line="478"/>
+        <source>Подкасты</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="231"/>
-        <source>play</source>
+        <location filename="mainwindow.ui" line="380"/>
+        <source>Исполнители</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="376"/>
-        <source>plus</source>
+        <location filename="mainwindow.ui" line="390"/>
+        <source>Плейлисты</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="78"/>
-        <source>step-forward</source>
+        <location filename="mainwindow.ui" line="400"/>
+        <source>Альбомы</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="101"/>
-        <source>volume-up</source>
+        <location filename="mainwindow.ui" line="410"/>
+        <source>Музыка</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="258"/>
-        <source>angry</source>
+        <location filename="mainwindow.ui" line="424"/>
+        <source>Избранное</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="160"/>
-        <source>random</source>
+        <location filename="mainwindow.ui" line="438"/>
+        <source>Любимые треки</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="202"/>
-        <source>heart</source>
+        <location filename="mainwindow.ui" line="448"/>
+        <source>Исследуйте</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="347"/>
-        <source>step-backward</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="319"/>
-        <location filename="mainwindow.ui" line="386"/>
+        <location filename="mainwindow.ui" line="107"/>
+        <location filename="mainwindow.ui" line="174"/>
         <source>00:00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="124"/>
-        <source>redo</source>
+        <location filename="mainwindow.ui" line="134"/>
+        <source>Включить режим перемешивания</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>Добавить в избранное</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="187"/>
+        <source>Не предлагать этот трек</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="244"/>
+        <source>Повторять все треки</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="287"/>
+        <source>Аудио</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
